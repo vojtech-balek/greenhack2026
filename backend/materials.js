10031bdef11381757699fa7c5f944e75f08c2360
vojtech-balek <vojta.balek@gmail.com>
2026-06-06 11:13:09 +0200

    fix pdf gen

--- backend/materials.js
+++ backend/materials.js
@@ -526,7 +526,7 @@ async function htmlToPdfWithPuppeteer(html) {
     }
     browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: ["--no-sandbox", "--disable-setuid-sandbox",'--disable-dev-shm-usage'],
     });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
